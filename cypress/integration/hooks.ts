994a18717779cfb5262701e8553975d12e68243c
deepak bhatt <deepakbhattmits@gmail.com>
2021-07-10 12:16:04 +0530

    Create hooks.ts

---
+++ cypress/integration/hooks.ts
@@ -0,0 +1,56 @@
+/** @format */
+
+// describe('Example Test Suit', () => {
+// 	it('renders correctly', () => {
+// 		cy.log('should visit the website');
+// 	});
+
+// 	it('i love cypress', () => {
+// 		cy.log('I Love Cypress');
+// 	});
+// });
+// hooks
+//  before
+//  beforeEach
+//  after
+//  afterEach
+
+// describe('Login feature test', () => {
+// 	before(() => {
+// 		cy.log('Run the server locally');
+//         cy.
+// 	});
+// 	beforeEach(() => {
+// 		cy.log('Navigate to login page');
+// 	});
+// 	after(() => {
+// 		cy.log('Kill the server');
+// 	});
+// 	afterEach(function () {
+// 		cy.log('Works !');
+// 	});
+// 	it('Should visit the login page correctly', () => {
+// 		// cy.log('Run the server locally');
+// 		// cy.log('Navigate to login page');
+// 		cy.log('Website should have the correct URL');
+// 		// cy.log('Kill the server');
+// 	});
+// 	it('Should be able to login', () => {
+// 		// cy.log('Run the server locally');
+// 		// cy.log('Navigate to login page');
+// 		cy.log('Fill the username and password and login');
+// 		cy.log('Should navigate the login page');
+// 		// cy.log('Kill the server');
+// 	});
+// });
+
+// describe('suit', () => {
+// 	const sizes: string[] = ['macbook-13', 'iphone-x'];
+// 	sizes.forEach((size: any) => {
+// 		it('visit the cypress website correctly', () => {
+// 			// cy.viewport(500, 750);
+// 			cy.viewport(size);
+// 			cy.visit('/');
+// 		});
+// 	});
+// });
