c106e512112e56945d1f79fd1b02e135492a0cc2
deepakbhattmits <deepakbhattmits@gmail.com>
2021-10-17 08:32:36 +0530

    Update NewTodo.tsx

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,35 +1,43 @@
 /** @format */
 
-import { FC, FormEvent, useRef } from 'react';
-import './NewTodo.css';
+import { FC, FormEvent, useState } from 'react'
+import './NewTodo.css'
 
 interface INewTodoProp {
-	addTodo: (text: string) => void;
+  addTodo: (text: string) => void
 }
 const NewTodo: FC<INewTodoProp> = ({ addTodo }) => {
-	const textInputRef = useRef<HTMLInputElement>(null);
-	const handleSubmit = (event: FormEvent) => {
-		event.preventDefault();
-		const enteredInputRef = textInputRef.current!.value;
-		// console.log('text : ', enteredInputRef);
-		if (!!enteredInputRef) {
-			addTodo(enteredInputRef);
-		} else {
-			alert('Field empty');
-		}
-	};
-	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor='todo-text'>Enter task</label>
-					<input type='text' id='todo-text' ref={textInputRef} required />
-				</div>
-				<div>
-					<button type='submit'>Add</button>
-				</div>
-			</form>
-		</div>
-	);
-};
-export default NewTodo;
+  const [todo, setTodo] = useState<string>('')
+
+  const handleChange = (e: any) => {
+    const { value } = e.target
+    setTodo(value)
+  }
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+
+    addTodo(todo)
+    setTodo('')
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="todo-text">Enter task</label>
+          <input
+            type="text"
+            id="todo-text"
+            value={todo}
+            onChange={handleChange}
+            required
+            autoComplete="off"
+          />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+    </>
+  )
+}
+export default NewTodo
